3900840e6e1a12ec665210096a42fa72102a2b1a
Fadhil <fadhilnigo@gmail.com>
2025-01-30 14:44:54 +0700

    feat: landing page

--- src/app/_providers/ServerSideQueryProvider.tsx
+++ src/app/_providers/ServerSideQueryProvider.tsx
@@ -1,18 +1,17 @@
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 import { ReactNode } from 'react';
-import { COMMON_API_QUERY_OPTIONS, EQueryKey } from '~/@api/query';
-import { getMock } from '../_hooks/useGetMock';
+// import { COMMON_API_QUERY_OPTIONS, EQueryKey } from '~/@api/query';
 
 // List all and fetch API that wanted to be fetched on the server
 const ServerSideQueryProvider = async ({ children }: { children: ReactNode }) => {
   const queryClient = new QueryClient();
 
   // example
-  await queryClient.prefetchQuery({
-    queryKey: [EQueryKey.GET_ARTICLE_LIST_DATA],
-    queryFn: getMock,
-    ...COMMON_API_QUERY_OPTIONS,
-  });
+  // await queryClient.prefetchQuery({
+  //   queryKey: [EQueryKey.GET_ARTICLE_LIST_DATA],
+  //   queryFn: getMock,
+  //   ...COMMON_API_QUERY_OPTIONS,
+  // });
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
